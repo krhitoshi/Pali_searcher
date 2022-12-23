84acfd4da0961a67101740d348eadc51d3784891
Hitoshi Kurokawa <hitoshi@nextseed.jp>
2022-12-23 20:36:56 +0900

    Fix indent

--- pali_searcher.py
+++ pali_searcher.py
@@ -20,12 +20,12 @@ class PaliSearcher:
     __slots__ = ("static_dir_path", "target_text_groups", "mode")
 
     text_vols = ["Vin_I", "Vin_II", "Vin_III", "Vin_IV", "Vin_V",
-    "DN_I", "DN_II", "DN_III",
-    "MN_I", "MN_II", "MN_III",
-    "SN_I", "SN_II", "SN_III", "SN_IV", "SN_V",
-    "AN_I", "AN_II", "AN_III", "AN_IV", "AN_V",
-    "Khp", "Dhp", "Ud", "It", "Sn", "Pv", "Vm", "Th", "Thi", "J", "Nidd_I", "Nidd_II", "Paṭis_I", "Paṭis_II", "Ap", "Bv", "Cp",
-    "Dhs", "Vibh", "Dhātuk", "Pugg", "Kv", "Yam_I", "Yam_II", "Mil", "Vism", "Sp", "Ja_1", "Ja_2", "Ja_3", "Ja_4", "Ja_5", "Ja_6"]
+                 "DN_I", "DN_II", "DN_III",
+                 "MN_I", "MN_II", "MN_III",
+                 "SN_I", "SN_II", "SN_III", "SN_IV", "SN_V",
+                 "AN_I", "AN_II", "AN_III", "AN_IV", "AN_V",
+                 "Khp", "Dhp", "Ud", "It", "Sn", "Pv", "Vm", "Th", "Thi", "J", "Nidd_I", "Nidd_II", "Paṭis_I", "Paṭis_II", "Ap", "Bv", "Cp",
+                 "Dhs", "Vibh", "Dhātuk", "Pugg", "Kv", "Yam_I", "Yam_II", "Mil", "Vism", "Sp", "Ja_1", "Ja_2", "Ja_3", "Ja_4", "Ja_5", "Ja_6"]
 
     def __init__(self, static_dir_path, target_text_groups,
                  mode=PaliSearcherMode.Web):
